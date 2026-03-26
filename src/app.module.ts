bac5a9c10f233133723764e1201f578ba177d1f5
fuis18 <warfuis18larcz018@gmail.com>
2026-03-26 01:34:31 -0500

    push

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { PrismaModule } from './prisma/prisma.module.js';
 import { ProductsModule } from './products/products.module.js';
 import { SalesModule } from './sales/sales.module.js';
 import { UsersModule } from './users/users.module.js';
+import { HealthController } from './health.controller.js';
 
 @Module({
   imports: [PrismaModule, ProductsModule, SalesModule, UsersModule],
-  controllers: [],
+  controllers: [HealthController],
   providers: [],
 })
 export class AppModule {}
